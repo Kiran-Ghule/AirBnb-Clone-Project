a3f9d9adf4119e72d8657426517fed13fd50fb5f
Kiran-Ghule <ghulekiran1608@gmail.com>
2026-06-27 19:48:41 +0530

    New JPQL is added to remove room after date and with room

--- src/main/java/com/airbnb/project/repository/InventoryRepository.java
+++ src/main/java/com/airbnb/project/repository/InventoryRepository.java
@@ -1,7 +1,12 @@
 package com.airbnb.project.repository;
 
 import com.airbnb.project.entities.Inventory;
+import com.airbnb.project.entities.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public interface InventoryRepository extends JpaRepository<Inventory,Long> {
+    void deleteByDateAfterAndRoom(LocalDate date, Room room);
 }
